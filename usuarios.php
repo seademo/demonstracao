f224589c5dcd0b1e94d4c1019be07a7f5082a5bc
Rafael Damásio <rafael.damasio@seatecnologia.com.br>
2018-04-18 11:07:57 -0300

    Criado tabela em html

--- usuarios.php
+++ usuarios.php
@@ -15,26 +15,126 @@ if ($conn->connect_error) {
     die("Conexão ao banco de dados falhou: " . $conn->connect_error);
 }
 
-$result = $conn->query("SELECT * FROM usuarios");
-if ($result->num_rows > 0) {
-    echo "<table>";
-        echo "<tr>";
-            echo "<th>ID</th>";
-            echo "<th>Nome</th>";
-            echo "<th>Login</th>";
-        echo "</tr>";
-        while($row = $result->fetch_assoc()) {
-        echo "<tr>";
-            echo "<td>" . $row['id'] . "</td>";
-            echo "<td>" . $row['nome'] . "</td>";
-            echo "<td>" . $row['login'] . "</td>";
-        echo "</tr>";
-        }
-    echo "</table>";
-}
-else {
-    echo "0 results";
-}
+<html>
+<head>
+	<title>Displaying MySQL Data in HTML Table</title>
+	<style type="text/css">
+		body {
+			font-size: 15px;
+			color: #343d44;
+			font-family: "segoe-ui", "open-sans", tahoma, arial;
+			padding: 0;
+			margin: 0;
+		}
+		table {
+			margin: auto;
+			font-family: "Lucida Sans Unicode", "Lucida Grande", "Segoe Ui";
+			font-size: 12px;
+		}
+
+		h1 {
+			margin: 25px auto 0;
+			text-align: center;
+			text-transform: uppercase;
+			font-size: 17px;
+		}
+
+		table td {
+			transition: all .5s;
+		}
+		
+		/* Table */
+		.data-table {
+			border-collapse: collapse;
+			font-size: 14px;
+			min-width: 537px;
+		}
+
+		.data-table th, 
+		.data-table td {
+			border: 1px solid #e1edff;
+			padding: 7px 17px;
+		}
+		.data-table caption {
+			margin: 7px;
+		}
+
+		/* Table Header */
+		.data-table thead th {
+			background-color: #508abb;
+			color: #FFFFFF;
+			border-color: #6ea1cc !important;
+			text-transform: uppercase;
+		}
+
+		/* Table Body */
+		.data-table tbody td {
+			color: #353535;
+		}
+		.data-table tbody td:first-child,
+		.data-table tbody td:nth-child(4),
+		.data-table tbody td:last-child {
+			text-align: right;
+		}
+
+		.data-table tbody tr:nth-child(odd) td {
+			background-color: #f4fbff;
+		}
+		.data-table tbody tr:hover td {
+			background-color: #ffffa2;
+			border-color: #ffff0f;
+		}
+
+		/* Table Footer */
+		.data-table tfoot th {
+			background-color: #e5f5ff;
+			text-align: right;
+		}
+		.data-table tfoot th:first-child {
+			text-align: left;
+		}
+		.data-table tbody td:empty
+		{
+			background-color: #ffcccc;
+		}
+	</style>
+</head>
+<body>
+	<table class="data-table">
+		<caption class="title">Lista de usuários cadastrados!!!</caption>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>NOME</th>
+				<th>LOGIN</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		$no 	= 1;
+		$total 	= 0;
+		while ($row = mysqli_fetch_array($query))
+		{
+			$amount  = $row['amount'] == 0 ? '' : number_format($row['amount']);
+			echo '<tr>
+					<td>'.$no.'</td>
+					<td>'.$row['ID'].'</td>
+                    <td>'.$row['nome'].'</td>
+                    <td>'.$row['login'].'</td>
+				</tr>';
+			$total += $row['amount'];
+			$no++;
+		}?>
+		</tbody>
+		<tfoot>
+			<tr>
+				<th colspan="4">TOTAL</th>
+				<th><?=number_format($total)?></th>
+			</tr>
+		</tfoot>
+	</table>
+</body>
+</html>
 
 $conn->close();
 
